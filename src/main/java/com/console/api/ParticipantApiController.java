f5fcb53eb5f20cdb776fb03af546750ff600b624
Mohammad Badar Hashimi <32482095+mohbadar@users.noreply.github.com>
2019-03-18 18:50:21 +0430

    changed

--- src/main/java/com/console/api/ParticipantApiController.java
+++ src/main/java/com/console/api/ParticipantApiController.java
@@ -5,10 +5,32 @@
  */
 package com.console.api;
 
+import com.console.model.Participant;
+import com.console.service.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Dell
  */
+@RestController
+@RequestMapping(value = "/api")
 public class ParticipantApiController {
-    
+
+    @Autowired
+    private ParticipantService participantService;
+
+    @GetMapping("/participant-by-id")
+    public Participant getParanticipantById(@RequestParam("id") Long id) {
+        if (id == 0 || id == null) {
+            return null;
+        }
+        return participantService.getOne(id);
+    }
+
 }
--- src/main/java/com/console/config/ParamsConfig.java
+++ src/main/java/com/console/config/ParamsConfig.java
@@ -10,14 +10,13 @@ package com.console.config;
  * @author Dell
  */
 public class ParamsConfig {
-    
-    
+        
     //*********************************** Security Config Params ***********************//
     public static final String    TRUSTED_CLIENT_ID = "my-trusted-client";
     public static final String    TRUSTED_CLIENT_SECRET = "secret";
     public static final String    TOKEN_GENERATION_URL = "/oauth/token";
     public static final Integer   ACCESS_TOKEN_VALDITION_TIME = 60 * 60 * 10;
-    public static final String    SECURED_RESOURCE_URL = "/host/**";
+    public static final String    SECURED_RESOURCE_URL = "/api/**";
     public static final String[]  OAUTH_SECURITY_SCOPE = {"read", "write", "trust"};
     public static final String[]  OAUTH_SECURITY_AUTHORITIES = {"ROLE_CLIENT", "ROLE_TRUSTED_CLIENT"};
     public static final String    OAUTH_SECURITY_RESOURCE_ID = "oauth2-resource";
